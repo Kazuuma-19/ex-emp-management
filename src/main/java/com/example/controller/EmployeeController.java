30fadbecf8f6fa1ae07241844608ea576e6f35db
Kazuuma-19 <kazu15870@gmail.com>
2025-05-20 13:35:45 +0900

    feat: 扶養人数を更新

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -45,5 +46,22 @@ public class EmployeeController {
         Employee employee = employeeService.showDetail(Integer.parseInt(id));
         model.addAttribute("employee", employee);
         return "employee/detail";
+    }
+
+    /**
+     * 従業員の扶養人数を更新し、従業員一覧画面にリダイレクトする.
+     *
+     * @param form 従業員情報
+     * @return 従業員一覧画面
+     */
+    @PostMapping("/update")
+    public String update(UpdateEmployeeForm form) {
+        Integer id = Integer.parseInt(form.getId());
+        Integer dependentsCount = Integer.parseInt(form.getDependentsCount());
+        // 扶養人数をsetし更新
+        Employee employee = employeeService.showDetail(id);
+        employee.setDependentsCount(dependentsCount);
+        employeeService.update(employee);
+        return "redirect:/employee/showList";
     }
 }
